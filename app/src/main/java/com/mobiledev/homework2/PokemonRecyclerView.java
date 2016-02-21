26d01a30964c517bec26f75fc8742d2d26618022
Tomas Yanez <tyanez@lclark.edu>
2016-02-21 14:41:43 -0800

    blah more stuff

---
+++ app/src/main/java/com/mobiledev/homework2/PokemonRecyclerView.java
@@ -0,0 +1,13 @@
+package com.mobiledev.homework2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PokemonRecyclerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pokemon_recycler_view);
+    }
+}
