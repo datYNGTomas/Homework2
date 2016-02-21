f8bc29dfa375325c5f03b2e09cd40d4aa030ea1a
Tomas Yanez <tyanez@lclark.edu>
2016-02-21 15:26:43 -0800

    finally got it to scroll stuff

--- app/src/main/java/com/mobiledev/homework2/MainActivity.java
+++ app/src/main/java/com/mobiledev/homework2/MainActivity.java
@@ -1,14 +1,27 @@
 package com.mobiledev.homework2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    PokemonArrayAdapter mAdapter;
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homework_activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.homework_main_listview);
+
+        Pokedex pokedex = new Pokedex();
+
+        mAdapter = new PokemonArrayAdapter(this, R.id.homework_main_listview, pokedex.getPokemons());
+
+        listView.setAdapter(mAdapter);
+
     }
 
 
