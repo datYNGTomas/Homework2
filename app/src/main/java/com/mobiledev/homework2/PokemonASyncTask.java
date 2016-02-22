0326c227d993ef67c713d1e3789b9dbe1a7f6ec2
Tomas Yanez <tyanez@lclark.edu>
2016-02-21 16:50:57 -0800

    async update

--- app/src/main/java/com/mobiledev/homework2/PokemonASyncTask.java
+++ app/src/main/java/com/mobiledev/homework2/PokemonASyncTask.java
@@ -4,19 +4,93 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class PokemonASyncTask extends AsyncTask<String, Integer, JSONArray> {
 
+
     public static final String TAG = PokemonASyncTask.class.getSimpleName();
+
 
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
-        Log.d(TAG, "Pokedex 2000 Lite has been activited");
+        Log.d(TAG, "Started AsyncTask");
     }
 
     @Override
     protected JSONArray doInBackground(String... params) {
-        return null;
+
+        StringBuilder responseBuilder = new StringBuilder();
+        JSONArray jsonArray = null;
+        if (params.length == 0) {
+            return null;
+        }
+
+        String userId = params[0];
+
+        try {
+            URL url = new URL("https://api.github.com/users/" + userId + "/subscriptions");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+
+            InputStreamReader inputStream = new InputStreamReader(connection.getInputStream());
+            BufferedReader reader = new BufferedReader(inputStream);
+            String line;
+
+            if (isCancelled()) {
+                return null;
+            }
+            while ((line = reader.readLine()) != null) {
+                responseBuilder.append(line);
+
+                if (isCancelled()) {
+                    return null;
+                }
+            }
+
+            jsonArray = new JSONArray(responseBuilder.toString());
+
+            if (isCancelled()) {
+                return null;
+            }
+        } catch (IOException | JSONException e) {
+            Log.e(TAG, e.getLocalizedMessage());
+        }
+
+        return jsonArray;
+    }
+
+    @Override
+    protected void onCancelled(JSONArray jsonArray) {
+        super.onCancelled(jsonArray);
+        Log.d(TAG, "AsyncTask cancelled");
+    }
+
+    @Override
+    protected void onPostExecute(JSONArray jsonArray) {
+        super.onPostExecute(jsonArray);
+        if (jsonArray == null) {
+            Log.e(TAG, "Resulting JSON is null");
+        } else {
+            for (int i = 0; i < jsonArray.length(); i++) {
+                try {
+                    JSONObject subscription = jsonArray.getJSONObject(i);
+                    JSONObject owner = subscription.getJSONObject("owner");
+
+                    Log.d(TAG, owner.getString("login") + " owns " + subscription.getString("name"));
+                } catch (JSONException e) {
+                    Log.e(TAG, e.getLocalizedMessage());
+                }
+            }
+            Log.d(TAG, jsonArray.toString());
+        }
     }
 }
